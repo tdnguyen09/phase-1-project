5862b19f98142547adbeb92badebf8068b5f6fbb
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-03-01 01:10:13 +1100

    building function to handle missing input

--- index.js
+++ index.js
@@ -3,36 +3,52 @@ document.addEventListener('DOMContentLoaded', () => {
         let darkMode = document.body;
         darkMode.classList.toggle('dark-mode');
     })
+    document.getElementById('contact').addEventListener('invalid',function (){
+        alert("You must fill out the contact!");
+    })
     getItemsInfo()
     document.querySelector('#query-form').addEventListener('submit',handleSubmit)
     function handleSubmit(e){
         e.preventDefault()
+        //console.log(document.getElementById('name'))
         let information = {
             name: e.target.name.value,
             contact:e.target.contact.value,
             enquiry:e.target.enquiry.value
         }
         enquiryForm(information)
+        alterSubmit()
     }
 })
+// const capitalizeFirstLetter = function () {
+//     let string = document.getElementById('name').value
+//     let words = string.split(" ");
+//     let capitalizedString = words.map((word) => {
+//         return word[0].toUpperCase() + word.substring(1);
+//     })
+//     return capitalizedString.join(" ")}
+
 function getItemsInfo(){
     fetch('http://localhost:3000/item')
     .then(res => res.json())
     .then(itemsDatas => itemsDatas.forEach(item => itemSetup(item)));
 }
 function itemSetup(items){
-    let saleItems = document.createElement('li')
+    let saleItems = document.createElement('article')
     saleItems.className = "saleItems"
     saleItems.innerHTML = `
-        <img src="${items.image}" width = "200px" heigh="350px/>
+        <img src="${items.image}"/>
         <div class="content">
             <h4>${items.name}</h4>
             <p>Condition:${items.condition}</p>
             <p>Description:${items.description}</p>
-            <p>Price:${items.price}</p>
-        </div> 
-        `
+            <p>Price:${items.price}</p> 
+        </div>
+    `
     document.querySelector('#items-list').append(saleItems)
+}
+function alterSubmit(){
+    alert('Thank you')
 }
 function enquiryForm(information){
     fetch('http://localhost:3000/enquiry',{
@@ -43,6 +59,6 @@ function enquiryForm(information){
         },
         body:JSON.stringify(information)
     })
-    .then(res =>res.json())
+    .then(res => res.json())
     .then(info => console.log(info))
 }
